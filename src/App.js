7d24e24f8adc2d8abf73885e316c6ebc1dbdd078
Sithum-Ravishka <79629419+Sithum-Ravishka@users.noreply.github.com>
2022-05-02 17:37:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useContext } from "react";
+import { themeContext } from "./Context";
+import Navbar from "./components/Navbar/Navbar";
+import Intro from "./components/Intro/Intro";
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+      <Navbar />
+      <Intro />
     </div>
   );
 }
